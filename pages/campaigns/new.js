f2c77efaf237c1481c484fa0e91ece0a973b9f3d
COGKOG <melissaljl@gmail.com>
2018-12-10 18:23:47 +0800

    ..

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -153,15 +153,9 @@ class CampaignNew extends Component {
         method: "PUT",
         body: formData
       });
-<<<<<<< HEAD
       this.setState({start:Date.now()});
       while (this.state.time < 1000){
         this.setState({time: Date.now() - this.state.start});
-=======
-      this.setState({ start: Date.now() });
-      while (this.state.time < 1000) {
-        this.setState({ time: Date.now() - this.state.start });
->>>>>>> 096c723a4beeed89a7c58533e13f2e4a45df3a7f
       }
       Router.pushRoute("/campaigns/" + this.state.campaignAddress);
     } catch (err) {
